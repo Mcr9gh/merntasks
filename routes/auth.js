091be2a36c676ce3325623e9ac7bd96ed0a9c463
Mario <mcr9em@gmail.com>
2021-04-21 12:05:47 -0500

    fix autcontroller

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const router= express.Router();
-const AuthController = require("../controllers/AuthController");
+const AuthController = require("../controllers/authController");
 const {check} = require('express-validator')
 const auth =require("../middleware/auth");
 
